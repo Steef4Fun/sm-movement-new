737602be9ad289b38e52fb20bafd61fefb788f8f
[dyad] <stefandjvermeulen@gmail.com>
2025-10-11 14:46:06 +0200

    [dyad] Finally fixing the Calendar component - wrote 1 file(s)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -51,10 +51,8 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
-      }}
+      IconLeft={<ChevronLeft className="h-4 w-4" />}
+      IconRight={<ChevronRight className="h-4 w-4" />}
       {...props}
     />
   )
